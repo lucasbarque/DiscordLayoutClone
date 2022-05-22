dfda01d3a1061f80c7b5486376a4bfe35be484c0
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-05-22 14:04:32 -0400

    ChannelButton Component

---
+++ src/components/ChannelButton/ChannelButton.tsx
@@ -0,0 +1,22 @@
+import { Container, HashtagIcon, InviteIcon, SettingsIcon } from './styles';
+
+export interface Props {
+  channelName: string;
+  selected?: boolean;
+}
+
+export default function ChannelButton({ channelName, selected }: Props) {
+  return (
+    <Container className={selected && 'active'}>
+      <div>
+        <HashtagIcon />
+        <span>{channelName}</span>
+      </div>
+
+      <div>
+        <InviteIcon />
+        <SettingsIcon />
+      </div>
+    </Container>
+  );
+}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,7 @@
+import ChannelInfo from '../ChannelInfo/ChannelInfo';
+import ChannelList from '../ChannelList/ChannelList';
 import ServerList from '../ServerList/ServerList';
+import ServerName from '../ServerName/ServerName';
 
 import { Grid } from './styles';
 
@@ -6,6 +9,9 @@ export default function Layout() {
   return (
     <Grid>
       <ServerList />
+      <ServerName />
+      <ChannelInfo />
+      <ChannelList />
     </Grid>
   );
 }
