a71d1eb8ead780f112ad81b08d47aa6768f7a860
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-05-22 14:04:14 -0400

    ServerName Component

---
+++ src/components/ServerName/ServerName.tsx
@@ -0,0 +1,11 @@
+import { Container, Title, ExpandIcon } from './styles';
+
+export default function ServerName() {
+  return (
+    <Container>
+      <Title>Servidor do Lucas</Title>
+
+      <ExpandIcon />
+    </Container>
+  );
+}
